13455cb20281c86f90a9a5b4e0d690e9d3077643
shuvishu <shubhamv306@gmail.com>
2017-04-01 10:40:42 +0530

    firebase commit latest

--- app/src/main/java/com/example/sairam/firebase/MinistryAct.java
+++ app/src/main/java/com/example/sairam/firebase/MinistryAct.java
@@ -1,13 +1,24 @@
 package com.example.sairam.firebase;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MinistryAct extends AppCompatActivity {
-
+    Button Badsupevisor;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ministry);
+        Badsupevisor= (Button) findViewById(R.id.Baddsupervisor);
+        Badsupevisor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent addsupervisorIntent=new Intent(MinistryAct.this,newSupervisor.class);
+                startActivity(addsupervisorIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/sairam/firebase/newSupervisor.java
+++ app/src/main/java/com/example/sairam/firebase/newSupervisor.java
@@ -1,13 +1,26 @@
 package com.example.sairam.firebase;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class newSupervisor extends AppCompatActivity {
-
+    Button BaddSupervisor;
+    Button cancelAddSupervisor;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_supervisor);
+        BaddSupervisor= (Button) findViewById(R.id.Baddsupervisor);
+        cancelAddSupervisor= (Button) findViewById(R.id.BcancelAddSupervisor);
+        cancelAddSupervisor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cancelAddSupervisor=new Intent(newSupervisor.this,MinistryAct.class);
+                startActivity(cancelAddSupervisor);
+            }
+        });
     }
 }
